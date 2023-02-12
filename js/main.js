53ca3dd662142096c036cec027fe031151257a93
Kornel <kornelodoj.4@gmail.com>
2023-02-12 23:46:54 +0100

    Adding records function working properly

--- js/main.js
+++ js/main.js
@@ -1,18 +1,54 @@
 const incomeTitle = document.querySelector('.income-title');
 const expenseTitle = document.querySelector('.expenses-title');
-const income = document.querySelector('.income');
-const expense = document.querySelector('.expenses');
+const incomeWrapper = document.querySelector('.income');
+const expensesWrapper = document.querySelector('.expenses');
+const form  = document.querySelector('.add-form');
+let counter = 1;
 const incomeExpenseChange = () => {
     incomeTitle.style.opacity = '0.5';
     expenseTitle.style.opacity = '1';
-    income.style.display = 'none';
-    expense.style.display = 'block';
+    incomeWrapper.style.display = 'none';
+    expensesWrapper.style.display = 'block';
+    counter = 0;
 }
 const expenseIncomeChange = () => {
     incomeTitle.style.opacity = '1';
     expenseTitle.style.opacity = '0.5';
-    income.style.display = 'block';
-    expense.style.display = 'none';
+    incomeWrapper.style.display = 'block';
+    expensesWrapper.style.display = 'none';
+    counter = 1;
 }
+
 incomeTitle.addEventListener('click', expenseIncomeChange);
 expenseTitle.addEventListener('click', incomeExpenseChange);
+
+const incomeName = document.querySelector('.input-name');
+const incomeValue = document.querySelector('.input-value');
+const incomeAppend = document.querySelector('.add-income-wrapper');
+const expensesAppend = document.querySelector('.add-expenses-wrapper');
+const addButton = document.querySelector('.input-button');
+//add counter if income clicked add 1 etc.
+const addRecord = () => {
+    let name = incomeName.value;
+    let value = incomeValue.value;
+    let wrap = document.createElement('div');
+    wrap.className = 'added-to-list';
+    counter == 1 ? incomeAppend.appendChild(wrap) : expensesAppend.appendChild(wrap);
+    let paragraph = document.createElement('p');
+    paragraph.innerText = `${name} - ${value}zł`;
+    wrap.appendChild(paragraph);
+    let editDeleteWrap = document.createElement('div');
+    editDeleteWrap.className = 'edit-delete-wrapper';
+    wrap.appendChild(editDeleteWrap);
+    let editButton = document.createElement('button');
+    editButton.className = 'edit-button';
+    editButton.innerText = 'Edytuj';
+    editDeleteWrap.appendChild(editButton);
+    let deleteButton = document.createElement('button');
+    deleteButton.className = 'delete-button';
+    deleteButton.innerText = 'Usuń';
+    editDeleteWrap.appendChild(deleteButton);
+    form.reset();
+}
+
+addButton.addEventListener('click', addRecord);
